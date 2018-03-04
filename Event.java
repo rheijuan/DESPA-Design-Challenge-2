d306c5482082e6fac926f400ce6e4cd3ba2519ed
Rhei Justin Juan <32697771+rheijuan@users.noreply.github.com>
2018-03-04 13:54:10 +0800

    Has start time and end time

--- Event.java
+++ Event.java
@@ -1,9 +1,14 @@
-package model;
+package sample;
 
-import java.util.Calendar;
+public class Event extends Occurrence {
 
-public class Event extends Occurence{
+    public Event(int year, int month, int dayOfMonth, String name, Color color, Classification classification, int startHour, int startMin, int endHour, int endMin) {
+        super(year, month, dayOfMonth, name, color, classification, startHour, startMin, endHour, endMin);
+        setEndTime(endHour, endMin);
+    }
 
-
-
+    @Override
+    public void setEndTime(int endHour, int endMin) {
+        this.timeEnd = endHour + ":" + endMin;
+    }
 }
