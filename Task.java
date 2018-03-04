b4f97781815a5249edd5f42bc16ad44b60c82235
Rhei Justin Juan <32697771+rheijuan@users.noreply.github.com>
2018-03-04 13:54:28 +0800

    To-Do's (Has end time and start time)

---
+++ Task.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class Task extends Occurrence {
+
+    public Task(int year, int month, int dayOfMonth, String name, Color color, Classification classification, int startHour, int startMin, int endHour, int endMin) {
+        super(year, month, dayOfMonth, name, color, classification, startHour, startMin, endHour, endMin);
+        setEndTime(endHour, endMin);
+    }
+
+    @Override
+    public void setEndTime(int endHour, int endMin) {
+        if(endMin + 30 == 60)
+            this.timeEnd = (endHour + 1) + ":" + 00;
+        else
+            this.timeEnd = endHour + ":" + (endMin + 30);
+    }
+}
